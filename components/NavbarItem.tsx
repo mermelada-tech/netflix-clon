1d841731a627cace2ce1ee675eab225dba66e060
Pablo Pedraza <wensel84@gmail.com>
2023-04-25 16:32:08 -0300

    feat: add navbar

---
+++ components/NavbarItem.tsx
@@ -0,0 +1,12 @@
+interface NavbarItemProps {
+  label: string;
+}
+const NavbarItem: React.FC<NavbarItemProps> = ({ label }) => {
+  return (
+    <div className="text-white cursor-pointer hover:text-gray-300 transition">
+      {label}
+    </div>
+  );
+};
+
+export default NavbarItem;

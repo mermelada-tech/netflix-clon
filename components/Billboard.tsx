6c4e882ac9b05b8c5ccbe3048a806d23bd1f2d13
Alfonsina <alfonsina.l@gmail.com>
2023-05-24 19:57:08 -0400

    feat(billboard): add random movie route for billboard

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,8 +1,11 @@
+import useBillboard from '@/hooks/useBillboard';
 import React from 'react';
 
 import { AiOutlineInfoCircle } from 'react-icons/ai';
 
 const Billboard = () => {
+  const { data } = useBillboard();
+
   return (
     <div className="relative h-[56.25vw]">
       <video
@@ -10,17 +13,15 @@ const Billboard = () => {
         autoPlay
         muted
         loop
-        poster="https://upload.wikimedia.org/wikipedia/commons/7/70/Big.Buck.Bunny.-.Opening.Screen.png"
-        src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+        poster={data?.thumbnailUrl}
+        src={data?.videoUrl}
       ></video>
       <div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16 w-full">
         <p className="text-white text-1xl md:text-5xl h-full w-[50%] lg:text-6xl font-bold drop-shadow-xl">
-          Big Buck Bunny
+          {data?.title}
         </p>
         <p className="text-white text-[8px] md:text-lg mt-3 md:mt-8 w-[90%] md:w-[90%] lg:w-[50%] drop-shadow-xl">
-          Three rodents amuse themselves by harassing creatures of the forest.
-          However, when they mess with a bunny, he decides to teach them a
-          lesson.
+          {data?.description}
         </p>
         <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
           <button

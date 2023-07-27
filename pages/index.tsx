3291860176b716eeb5b602d9295f7fe331c12356
AitanaCodes <aitana.hernandezpina@gmail.com>
2023-07-27 11:15:24 +0200

    feat(protecting routes): added current, useCurrentUser and a Logout button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,36 @@
 import Billboard from '@/components/Billboard';
 import Navbar from '@/components/Navbar';
+import { NextPageContext } from 'next';
+import { signOut, getSession } from 'next-auth/react';
+import useCurrentUser from '@/hooks/useCurrentUser';
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
   return (
     <>
       <Navbar />
       <Billboard />
+      <p className="text-white">Logged in as: {user?.email}</p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        Logout!
+      </button>
     </>
   );
 }

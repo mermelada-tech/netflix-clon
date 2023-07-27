96ea22a77cfeeaef6a11c13255775ecbfa1c0d1c
AitanaCodes <aitana.hernandezpina@gmail.com>
2023-07-27 13:54:58 +0200

    feat(profiles): added a selector of profiles after user authentication

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -6,10 +6,8 @@ import React from 'react';
 import { signIn } from 'next-auth/react';
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
-import { useRouter } from 'next/router';
 
 const Auth = () => {
-  const router = useRouter();
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
@@ -38,14 +36,12 @@ const Auth = () => {
       await signIn('credentials', {
         email,
         password,
-        redirect: false,
-        callbackUrl: '/',
+        callbackUrl: '/profiles',
       });
-      router.push('/');
     } catch (error) {
       console.log(error);
     }
-  }, [email, password, router]);
+  }, [email, password]);
 
   const register = useCallback(async () => {
     try {
@@ -109,13 +105,13 @@ const Auth = () => {
             </button>
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
               <div
-                onClick={() => signIn('google', { callbackUrl: '/' })}
+                onClick={() => signIn('google', { callbackUrl: '/profiles' })}
                 className="w-10 h-10 bg-white rounded-full flex  items-center justify-center cursor-pointer hover:opacity-80 transition"
               >
                 <FcGoogle size={30} />
               </div>
               <div
-                onClick={() => signIn('github', { callbackUrl: '/' })}
+                onClick={() => signIn('github', { callbackUrl: '/profiles' })}
                 className="w-10 h-10 bg-white rounded-full flex  items-center justify-center cursor-pointer hover:opacity-80 transition"
               >
                 <FaGithub size={30} />
